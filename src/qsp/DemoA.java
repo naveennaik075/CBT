c6a8bc087f0c72c7487141fef810da8fa30c930a
naveennaik075 <naveennaik075@gmail.com>
2016-07-10 14:02:46 +0530

    Update DemoA.java
    Updated from github second time

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -5,6 +5,7 @@ public class DemoA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// This is editing from github.com
+		// Editing second time from github.com
       System.out.println("Test method");
 	}
 
