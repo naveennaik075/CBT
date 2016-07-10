20f1357cf34e934b5e5d2bbcc17b5173c2042104
Naveen Naik <naveennaik075@gmail.com>
2016-07-10 14:09:35 +0530

    This commit is from eclipse

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -6,6 +6,7 @@ public class DemoA {
 		// TODO Auto-generated method stub
 		// This is editing from github.com
 		// Editing second time from github.com
+		// Edited third time from Eclipse 
       System.out.println("Test method");
 	}
 
