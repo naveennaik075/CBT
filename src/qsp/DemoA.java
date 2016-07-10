c07d3039c2cfcf34bdfc913854a6b3557a0644d5
Naveen Naik <naveennaik075@gmail.com>
2016-07-10 14:37:14 +0530

    Commited after second method

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -6,6 +6,8 @@ public class DemoA {
 		// TODO Auto-generated method stub
 		// This is editing from github.com
       System.out.println("Test method");
+      
+      System.out.println("test method second time");
 	}
 
 }
